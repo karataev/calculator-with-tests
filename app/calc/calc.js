a3998406650b9e268a00c8b47be120a478c0ed3c
karataev <eugene.karataev@gmail.com>
2016-07-28 00:23:10 +0700

    Test fails after adding new functionality and refactoring. Awesome!

--- app/calc/calc.js
+++ app/calc/calc.js
@@ -2,13 +2,29 @@ calcApp
   .component('calc', {
     controller: [function() {
       var vm = this;
-      
+
+      var PLUS = '+';
+      var MINUS = '-';
+
       vm.a = 1;
       vm.b = 1;
       vm.result = undefined;
       
-      vm.add = function() {
-        vm.result = Number(vm.a) + Number(vm.b);
+      vm.operations = [
+        {title: PLUS},
+        {title: MINUS}
+      ];
+      vm.selectedOperation = vm.operations[0];
+      
+      vm.calc = function() {
+        var aNum = Number(vm.a);
+        var bNum = Number(vm.b);
+        if (vm.selectedOperation.title === PLUS) {
+          vm.result = aNum + bNum;
+        } else if (vm.selectedOperation.title === MINUS) {
+          vm.result = aNum - bNum;
+        }
+
       }
     }],
     templateUrl: 'calc/calc.html'
