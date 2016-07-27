ffde005bafd3ec4e661ab823692900bbbb6bd312
karataev <eugene.karataev@gmail.com>
2016-07-27 22:53:18 +0700

    Calculator is capable to add two numbers! How cool is that?

--- app/calc/calc.js
+++ app/calc/calc.js
@@ -2,7 +2,14 @@ calcApp
   .component('calc', {
     controller: [function() {
       var vm = this;
-
+      
+      vm.a = 1;
+      vm.b = 1;
+      vm.result = undefined;
+      
+      vm.calc = function() {
+        vm.result = Number(vm.a) + Number(vm.b);
+      }
     }],
     templateUrl: 'calc/calc.html'
   })
