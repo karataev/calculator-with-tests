154ae154645019ef093bd29f6af3f0dc0ab45044
karataev <eugene.karataev@gmail.com>
2016-07-28 00:01:34 +0700

    New calc component test: controller should be attached to scope

--- app/calc/calc.spec.js
+++ app/calc/calc.spec.js
@@ -1,9 +1,23 @@
 'use strict';
 describe('Calc component', function() {
 
-  it('should be the first test', function() {
-    var a = 1;
-    var b = 1;
-    expect(a).toEqual(b);
-  })
+  var controller;
+  var scope;
+  beforeEach(module('calcApp'));
+  beforeEach(inject(function($rootScope, $componentController){
+    scope = $rootScope.$new();
+    controller = $componentController('calc', {$scope: scope});
+  }));
+
+  it('should be attached to the scope', function() {
+    expect(scope.$ctrl).toBe(controller);
+  });
+
+  /*
+    it('should be the first test', function() {
+      var a = 1;
+      var b = 1;
+      expect(a).toEqual(b);
+    })
+  */
 })
