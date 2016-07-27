92c3c65fb85b3ff9a468c31d316b21d863cf9414
karataev <eugene.karataev@gmail.com>
2016-07-27 22:43:45 +0700

    Add angular with blank calc component

---
+++ app/app.js
@@ -0,0 +1,2 @@
+
+var calcApp = angular.module('calcApp', []);
---
+++ app/calc/calc.js
@@ -0,0 +1,8 @@
+calcApp
+  .component('calc', {
+    controller: [function() {
+      var vm = this;
+
+    }],
+    templateUrl: 'calc/calc.html'
+  })
